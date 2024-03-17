fe3aa60bfd17bb6f89db4c6f87817adba274d072
Rushikesh1715 <121252476+Rushikesh1715@users.noreply.github.com>
2024-03-17 11:47:00 +0530

    add OAuth Functionality

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  //  apiKey: "AIzaSyBpoRcBEg6Xf_QBPNqJ5GHFU_4q2FdRdM8",
+   apiKey : import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "journeyjot-d0395.firebaseapp.com",
+  projectId: "journeyjot-d0395",
+  storageBucket: "journeyjot-d0395.appspot.com",
+  messagingSenderId: "851685098846",  
+  appId: "1:851685098846:web:4b1d534d5c13e79423fbcf"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
