f4d172375ebc1aab23aaa902dd49a40bab01bfc7
Rushikesh1715 <121252476+Rushikesh1715@users.noreply.github.com>
2024-03-18 16:45:39 +0530

    admin funtionality is add to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,33 +36,37 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email || !password || email === "" || password === "") {
-   return next(errorHandler(400, "All fields are required"));
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
   }
+
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      next(errorHandler(400, "User not found"));
+      return next(errorHandler(404, 'User not found'));
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
-     return next(errorHandler(400, "Invalid password"));
+      return next(errorHandler(400, 'Invalid password'));
     }
     const token = jwt.sign(
-        {id:validUser._id},process.env.JWT_SECRET)
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
-        const {password:pass , ...rest} =validUser._doc;
+    const { password: pass, ...rest } = validUser._doc;
 
-        res
-        .status(200)
-        .cookie('access_token', token, {
-          httpOnly: true,
-        })
-        .json(rest);
-    } catch (error) {
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
+
 export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;
   try {
