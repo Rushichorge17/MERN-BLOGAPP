3ff226302ec4515e4cbb3afe6e078e017ed77dc4
Rushikesh1715 <121252476+Rushikesh1715@users.noreply.github.com>
2024-03-19 14:41:05 +0530

    emoji add

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -37,7 +37,7 @@ export default function CreatePost() {
 
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
-      <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+      <h1 className="text-center text-3xl my-7 font-semibold">Create a post📝</h1>
       <form className="flex flex-col gap-4">
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
           <TextInput
