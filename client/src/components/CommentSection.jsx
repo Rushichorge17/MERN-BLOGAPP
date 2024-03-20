843abe820211566152dff8d6a7a293e44b92dd9b
Rushikesh1715 <121252476+Rushikesh1715@users.noreply.github.com>
2024-03-20 12:48:53 +0530

    show commect add the post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Alert, Button, Textarea } from 'flowbite-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types'; // Import PropTypes
+import Comment from './Comment';
 
 
 export default function CommentSection({ postId }) {
@@ -39,6 +40,21 @@ export default function CommentSection({ postId }) {
           setCommentError(error.message);
         }
       };
+
+      useEffect(() => {
+        const getComments = async () => {
+          try {
+            const res = await fetch(`/api/comment/getPostComments/${postId}`);
+            if (res.ok) {
+              const data = await res.json();
+              setComments(data);
+            }
+          } catch (error) {
+            console.log(error.message);
+          }
+        };
+        getComments();
+      }, [postId]);
 
     return (
         <div className='max-w-2xl mx-auto w-full p-3'>
@@ -91,6 +107,30 @@ export default function CommentSection({ postId }) {
             </Alert>
           )}
         </form>
+      )}
+       {comments.length === 0 ? (
+        <p className='text-sm my-5'>No comments yet!</p>
+      ) : (
+        <>
+          <div className='text-sm my-5 flex items-center gap-1'>
+            <p>Comments</p>
+            <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+              <p>{comments.length}</p>
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment
+              key={comment._id}
+              comment={comment}
+            //   onLike={handleLike}
+            //   onEdit={handleEdit}
+            //   onDelete={(commentId) => {
+            //     setShowModal(true);
+            //     setCommentToDelete(commentId);
+            //   }}
+           />
+          ))}
+        </>
       )}
         </div>
   )
